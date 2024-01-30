eb58222704c503a947b585f53eb3e17635729584
pharreller <phafoubastien@gmail.com>
2024-01-30 20:55:28 +0100

    Audiomanager script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource[] audioSources;
+    public float delay;
+    private int previouslyPlayed;
+
+    private void Start()
+    {
+        audioSources=GetComponents<AudioSource>();
+    }
+
+    IEnumerator PlayRandomAudio()
+    {
+        int randIndex = Random.Range(0, audioSources.Length - 1);
+        if (previouslyPlayed!=null && randIndex == previouslyPlayed)
+        {
+            if (audioSources[randIndex-1]!=null)
+            {
+                randIndex -= 1;
+            }
+            else
+            {
+                randIndex += 1;
+            }
+        }
+        audioSources[randIndex].Play();
+        previouslyPlayed = randIndex;
+        yield return new WaitForSeconds(delay);
+    }
+}
