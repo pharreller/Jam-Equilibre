9fd854600f5acf9db26d7ac63a71af675b606e84
pharreller <phafoubastien@gmail.com>
2024-01-30 11:49:54 +0100

    carnet wip

--- Assets/Scripts/Items/Carnet.cs
+++ Assets/Scripts/Items/Carnet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,25 @@ public class Carnet : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray;
+    private bool carnetIsVisible = false;
+
+    private void Update()
+    {
+        if (!carnetIsVisible && Input.GetKeyDown(KeyCode.E))
+        {
+            Sho
+        }
+    }
+
     void ChangeSprite()
     {
         spriteRenderer.sprite = spriteArray[0]; 
     }
+
+    void ShowCarnet()
+    {
+        
+    }
+    
+    
 }
