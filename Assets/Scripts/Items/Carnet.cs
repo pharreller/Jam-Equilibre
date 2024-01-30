f1fb60fb99d8f037711afed9783a852d252acd48
pharreller <phafoubastien@gmail.com>
2024-01-30 12:21:25 +0100

    carnet script

--- Assets/Scripts/Items/Carnet.cs
+++ Assets/Scripts/Items/Carnet.cs
@@ -5,26 +5,72 @@ using UnityEngine;
 
 public class Carnet : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite[] spriteArray;
+    public SpriteRenderer coverRenderer;
+    public SpriteRenderer leftPageRenderer;
+    public SpriteRenderer rightPageRenderer;
+    public Sprite[] pagesArray;
     private bool carnetIsVisible = false;
+    private int leftPageVisible = 0;
 
     private void Update()
     {
-        if (!carnetIsVisible && Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Sho
+            ToggleShowCarnet();
+        }
+        
+        if (carnetIsVisible)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (leftPageVisible > 0)
+                {
+                    leftPageVisible -= 1;
+                }
+                else
+                {
+                    Debug.Log("No pages before");
+                }
+
+                TurnPage();
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                if (leftPageVisible < 3)
+                {
+                    leftPageVisible += 1;
+                }
+                else
+                {
+                    Debug.Log("No pages after");
+                }
+                TurnPage();
+            }
+        }
+    }
+    
+    void ToggleShowCarnet()
+    {
+        if (carnetIsVisible)
+        {
+            carnetIsVisible = false;
+            coverRenderer.enabled = false;
+            leftPageRenderer.enabled = false;
+            rightPageRenderer.enabled = false;
+        }
+        else
+        { 
+            carnetIsVisible = true;
+            coverRenderer.enabled = true;
+            leftPageRenderer.enabled = true;
+            rightPageRenderer.enabled = true;
         }
     }
 
-    void ChangeSprite()
+    void TurnPage()
     {
-        spriteRenderer.sprite = spriteArray[0]; 
-    }
-
-    void ShowCarnet()
-    {
-        
+        leftPageRenderer.sprite = pagesArray[leftPageVisible];
+        rightPageRenderer.sprite = pagesArray[leftPageVisible+1];
     }
     
     
